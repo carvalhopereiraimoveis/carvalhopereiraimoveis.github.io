4c613be0d28976402d94a001c3f794c1434ded47
Neto <rodolfotiago@gmail.com>
2016-10-18 13:05:00 -0200

    oi

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
-var myApp = angular.module('myApp', ['dx']);
-myApp.controller("demoController", function ($scope) {
-    $scope.actionSheetVisible = false;
-    $scope.actionSheetCancelText = "Cancel";
-    $scope.actionSheetData = [
-        { text: "Reply" },
-        { text: "ReplyAll" },
-        { text: "Forward" },
-        { text: "Delete" }
-    ];
-    $scope.showActionSheet = function () {
-        $scope.actionSheetVisible = true;
-    };
-});
-angular.element(document).ready(function () {
-    angular.bootstrap(document, ['myApp']);
-});
+  $(document).ready(function(){
+    $('ul.tabs').tabs();
+    $('ul.tabs').tabs('select_tab', 'tab_id');
+    
+  });
