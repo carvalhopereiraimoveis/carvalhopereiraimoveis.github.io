ad3336afae3ea9784a9d96137fbae77c7972dc7f
Neto <rodolfotiago@gmail.com>
2016-11-07 13:07:10 -0200

    ola

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,22 @@ $( document ).ready(function() {
       interval: 4200
     })
 
+
+    var $btns = $('.btn').click(function() {
+	  if (this.id == 'todos') {
+	    $('#parent > div').fadeIn(450);
+	  } else {
+	    var $el = $('.' + this.id).fadeIn(450);
+	    $('#parent > div').not($el).fadeOut(450);
+	  }
+	  $btns.removeClass('ativo');
+	  $(this).addClass('ativo');
+	})
+
+
+
+
+
+
+
 });
